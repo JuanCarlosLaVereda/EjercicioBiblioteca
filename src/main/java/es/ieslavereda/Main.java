88aae2410090853a744640fff284195686cb0234
JuanCarlosLaVereda <juasanper2@alu.edu.gva.es>
2024-01-12 22:00:50 +0100

    v.Final

--- src/main/java/es/ieslavereda/Main.java
+++ src/main/java/es/ieslavereda/Main.java
@@ -8,12 +8,14 @@ public class Main {
             biblioteca1.addLibro(new Libro("Ejemplo" + i, "Autor" + i, 33));
         }
         System.out.println(biblioteca2.removeLibro(new Libro("Don Quijote de la Mancha", "Miguel de Cervantes Saavedra", 3)));
-        biblioteca2.removeLibro(new Libro("El amor en los tiempos del cólera", "Gabriel García Márquez", 3));
-        biblioteca2.removeLibro(new Libro("Desolación", "Gabriela Mistral", 3));
-        biblioteca2.removeLibro(new Libro("dfgdfgdfgfgfg", "Ggggggggg", 3));
+        System.out.println(biblioteca2.removeLibro(new Libro("El amor en los tiempos del cólera", "Gabriel García Márquez", 3)));
+        System.out.println(biblioteca2.removeLibro(new Libro("Desolación", "Gabriela Mistral", 3)));
+        System.out.println(biblioteca2.removeLibro(new Libro("dfgdfgdfgfgfg", "Ggggggggg", 3)));
 
-        biblioteca1.prestamo(new Libro("Don Quijote de la Mancha", "Miguel de Cervantes Saavedra", 3));
-        biblioteca2.prestamo(new Libro("Don Quijote de la Mancha", "Miguel de Cervantes Saavedra", 3));
+
+
+        System.out.println(biblioteca1.prestamo(new Libro("Don Quijote de la Mancha", "Miguel de Cervantes Saavedra", 3)));
+        System.out.println(biblioteca2.prestamo(new Libro("Don Quijote de la Mancha", "Miguel de Cervantes Saavedra", 3)));
         System.out.println(biblioteca1);
         System.out.println(biblioteca2);
     }
